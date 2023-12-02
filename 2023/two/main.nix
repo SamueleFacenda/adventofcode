91bb6f6df135869599b00d57e9f459a3d06f653d
SamueleFacenda <samuele.facenda@gmail.com>
2023-12-02 23:19:25 +0100

    Code formatting
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- 2023/two/main.nix
+++ 2023/two/main.nix
@@ -1,5 +1,5 @@
 # run with 'nix-instantiate --eval main.nix'
-let 
+let
   lib = (builtins.getFlake "nixpkgs").legacyPackages.${builtins.currentSystem}.lib;
   inherit (builtins) readFile map filter isInt head match stringLength split isList listToAttrs tail all;
   inherit (lib.strings) splitString toInt stringToCharacters;
@@ -7,43 +7,47 @@ let
   inherit (lib.attrsets) nameValuePair attrByPath;
   inherit (lib.debug) traceVal traceValFn;
   fileLines = file: filter
-                      (str: (stringLength str)>0)
-                      (splitString "\n" (readFile file));
+    (str: (stringLength str) > 0)
+    (splitString "\n" (readFile file));
   # return a list of lists
   splitList = split: list: fold
-    (cur: prev: if cur==split
-      then [[]] ++ prev
-      else [((head prev) ++ [cur])] ++ (tail prev)) # append to first list
-    [[]] list;
+    (cur: prev:
+      if cur == split
+      then [ [ ] ] ++ prev # add empty list on top
+      else [ ((head prev) ++ [ cur ]) ] ++ (tail prev)) # append to first list
+    [ [ ] ]
+    list;
 in
 # sum indexes
 fold (a: b: (traceVal (head a)) + b) 0
   # get only valid lines
   (filter
     (line: all
-      (draw: 
-        (attrByPath ["red"] 0 draw) <= 12 && 
-        (attrByPath ["green"] 0 draw) <= 13 && 
-        (attrByPath ["blue"] 0 draw) <= 14)
+      (draw:
+      (attrByPath [ "red" ] 0 draw) <= 12 &&
+      (attrByPath [ "green" ] 0 draw) <= 13 &&
+      (attrByPath [ "blue" ] 0 draw) <= 14)
+      
       (tail line)) # first element is id
     # convert lines to [id {blue=12} {red=15;blue=0;}]
     (map
       # x: every row
-      (x: let
-          # raw text to [id num color ; ...]
-          groups = flatten (filter isList (split "([0-9]+|red|green|blue|;)" x));
-        in
-          # [id num color num color ; num color ; num color] -> [id {color=num} {color=num;color=num}]
-          [(toInt (head groups))] ++ # first element is id
-          # [[num color] [num color num color]] -> [{color=num}...]
-          (map
-            # [num color num color] -> {color=num;color=num}
-            (group: (listToAttrs 
-              (zipListsWith
-                nameValuePair
-                (filter (x: (match "^[0-9]+$" x) == null) group)
-                (map toInt (filter (x: (match "^[0-9]+$" x) != null) group)))))
-            # [id num color ; num color num color] -> [[num color] [num color num color]]
-            (splitList ";" (tail groups)) ) )
-      
+      (x:
+      let
+        # raw text to [id num color ; ...]
+        groups = flatten (filter isList (split "([0-9]+|red|green|blue|;)" x));
+      in
+      # [id num color num color ; num color ; num color] -> [id {color=num} {color=num;color=num}]
+      [ (toInt (head groups)) ] ++ # first element is id
+        # [[num color] [num color num color]] -> [{color=num}...]
+      (map
+        # [num color num color] -> {color=num;color=num}
+        (group: (listToAttrs
+          (zipListsWith
+            nameValuePair
+            (filter (x: (match "^[0-9]+$" x) == null) group)
+            (map toInt (filter (x: (match "^[0-9]+$" x) != null) group)))))
+        # [id num color ; num color num color] -> [[num color] [num color num color]]
+        (splitList ";" (tail groups))))
+
       (fileLines ./inputtest)))
--- 2023/two/main2.nix
+++ 2023/two/main2.nix
@@ -1,5 +1,5 @@
 # run with 'nix-instantiate --eval main.nix'
-let 
+let
   lib = (builtins.getFlake "nixpkgs").legacyPackages.${builtins.currentSystem}.lib;
   inherit (builtins) readFile map filter isInt head match stringLength split isList listToAttrs tail all toString;
   inherit (lib.strings) splitString toInt stringToCharacters;
@@ -8,43 +8,45 @@ let
   inherit (lib.debug) traceVal traceValFn;
   inherit (lib.trivial) max;
   fileLines = file: filter
-                      (str: (stringLength str)>0)
-                      (splitString "\n" (readFile file));
+    (str: (stringLength str) > 0)
+    (splitString "\n" (readFile file));
   # return a list of lists
   splitList = split: list: fold
-    (cur: prev: if cur==split
-      then [[]] ++ prev
-      else [((head prev) ++ [cur])] ++ (tail prev)) # append to first list
-    [[]] list;
+    (cur: prev:
+      if cur == split
+      then [ [ ] ] ++ prev
+      else [ ((head prev) ++ [ cur ]) ] ++ (tail prev)) # append to first list
+    [ [ ] ]
+    list;
 in
 # sum all the values
 fold (a: b: a + b) 0
   # map line to minimum power
   (map
     # line: [{color=num} {color=num;color=num}]
-    (line: 
-      let
-        getMaxAttr = attr: let attrs = catAttrs attr line; in  fold max (head attrs) attrs;
-      in
-        (getMaxAttr "blue") * (getMaxAttr "red") * (getMaxAttr "green"))
-        
+    (line:
+    let
+      getMaxAttr = attr: let attrs = catAttrs attr line; in fold max (head attrs) attrs;
+    in
+    (getMaxAttr "blue") * (getMaxAttr "red") * (getMaxAttr "green"))
+
     # convert lines to [{blue=12} {red=15;blue=0;}]
     (map
-      (row: 
-          # [[num color] [num color num color]] -> [{color=num}...]
-          (map
-            # [num color num color] -> {color=num;color=num}
-            (group: (listToAttrs 
-              (zipListsWith
-                nameValuePair
-                (filter (x: (match "^[0-9]+$" x) == null) group)
-                (map toInt (filter (x: (match "^[0-9]+$" x) != null) group)))))
-            # [num color ; num color num color] -> [[num color] [num color num color]]
-            (splitList ";" 
-              (tail # remove id
-                (flatten # get ["id" "num" "color" ";" ...]
-                  (filter isList 
-                    # text to [["id"] "something" ["num"] ["color"] ...]
-                    (split "([0-9]+|red|green|blue|;)" row))))) ) )
-      
+      (row:
+      # [[num color] [num color num color]] -> [{color=num}...]
+      (map
+        # [num color num color] -> {color=num;color=num}
+        (group: (listToAttrs
+          (zipListsWith
+            nameValuePair
+            (filter (x: (match "^[0-9]+$" x) == null) group)
+            (map toInt (filter (x: (match "^[0-9]+$" x) != null) group)))))
+        # [num color ; num color num color] -> [[num color] [num color num color]]
+        (splitList ";"
+          (tail # remove id
+            (flatten # get ["id" "num" "color" ";" ...]
+              (filter isList
+                # text to [["id"] "something" ["num"] ["color"] ...]
+                (split "([0-9]+|red|green|blue|;)" row)))))))
+
       (fileLines ./input)))
