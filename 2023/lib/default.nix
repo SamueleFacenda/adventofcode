89c0e259a03c01c4d3915d2ac805843015ca298b
SamueleFacenda <samuele.facenda@gmail.com>
2023-12-04 12:10:31 +0100

    Day three part 1 2023
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- 2023/lib/default.nix
+++ 2023/lib/default.nix
@@ -1,12 +1,12 @@
 # utils to import
 rec {
   lib = (builtins.getFlake "nixpkgs").legacyPackages.${builtins.currentSystem}.lib;
-  inherit (builtins) readFile map filter isInt head match stringLength split isList listToAttrs tail all toString tryEval elemAt lenght;
-  inherit (lib.strings) splitString toInt stringToCharacters;
-  inherit (lib.lists) fold last flatten zipListsWith;
+  inherit (builtins) readFile map filter isInt head match stringLength split isList listToAttrs tail all toString tryEval elemAt length;
+  inherit (lib.strings) splitString toInt stringToCharacters fixedWidthNumber;
+  inherit (lib.lists) fold last flatten zipListsWith imap0 subtractLists;
   inherit (lib.attrsets) nameValuePair attrByPath catAttrs cartesianProductOfSets;
   inherit (lib.debug) traceVal traceValFn;
-  inherit (lib.trivial) max;
+  inherit (lib.trivial) max boolToString;
   fileLines = file: filter
     (str: (stringLength str) > 0)
     (splitString "\n" (readFile file));
@@ -18,7 +18,9 @@ rec {
       else [ ((head prev) ++ [ cur ]) ] ++ (tail prev)) # append to first list
     [ [ ] ]
     list;
-  sum = fold (a:b: a+b) 0;
+  sum = fold (a: b: a+b) 0;
   minAttr = list: attr: let attrs = catAttrs attr list; in fold max (head attrs) attrs;
-  tryOrDefault: default: expr: let res = tryEval expr; in if res.success then res.value else default;
+  tryOrDefault = default: expr: let res = tryEval expr; in if res.success then res.value else default;
+  traceBoolList = traceValFn (y: toString (map (x: if x then "#" else ".") y));
+  traceBoolListPrefixed = pre: traceValFn (y: toString ([pre] ++ (map (x: if x then "#" else ".") y)));
 }
